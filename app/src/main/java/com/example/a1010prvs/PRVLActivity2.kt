bfda66ee8a18eba3f7bc5e8fb30f56c8dd0dcdbf
DESKTOP-D4D5HOK\Lab-ERP-30 <ringguidfa@gmail.com>
2022-10-29 14:47:30 +0700

    last commit

---
+++ app/src/main/java/com/example/a1010prvs/PRVLActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.a1010prvs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PRVLActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prvlactivity2)
+    }
+}
